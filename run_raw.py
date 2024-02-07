40cd1f46ca0650edf53d00aeb4ba5b9ba5e2d2c4
Alessandro1918 <alessandro1918@hotmail.com>
2024-02-07 18:21:46 -0300

    refactor: use MVC architecture, padronize http req/res

--- run_raw.py
+++ run_raw.py
@@ -25,4 +25,4 @@ def create_tag():
     # return send_file(filepath, mimetype='image/gif')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=4000, debug=True)
